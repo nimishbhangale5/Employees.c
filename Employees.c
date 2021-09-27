2fccd9ddb2d6cff41f232abc5ade5bea387d3125
nimishbhangale5 <91449680+nimishbhangale5@users.noreply.github.com>
2021-09-27 19:09:58 +0530

    Create Employees.c
    .

---
+++ Employees.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+    char name[30];
+    int age;
+    int PhoneNumber;
+    double salary;
+} Employee;
+ 
+int main()
+{
+    int n=5;
+
+    Employee employees[n];
+ 
+    printf("Enter %d Employee Details \n \n",n);
+    for(int i=0; i<n; i++){
+        printf("Employee %d:- \n",i+1);
+
+        printf("Name: ");
+        scanf("%[^\n]s",employees[i].name);
+
+        printf("Age: ");
+        scanf("%d",&employees[i].age);
+        
+        printf("Phone Number: ");
+        scanf("%d",&employees[i].PhoneNumber);
+
+        printf("Salary: ");
+        scanf("%lf",&employees[i].salary);
+
+        char ch = getchar();
+ 
+        printf("\n");
+    }
+ 
+    printf("-------------- All Employees Details ---------------\n");
+    for(int i=0; i<n; i++){
+        printf("Name \t: ");
+        printf("%s \n",employees[i].name);
+ 
+        printf("Age \t: ");
+        printf("%d \n",employees[i].age);
+        
+        printf("Phone Number \t: ");
+        printf("%d \n",employees[i].PhoneNumber);
+ 
+        printf("Salary \t: ");
+        printf("%.2lf \n",employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+}
